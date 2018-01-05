b3c201c99a9e0091191f42a03220c3546e06227b
Sylvain Ung <sy-lux@hotmail.fr>
2018-01-05 15:44:04 +0100

    update pom.xml

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -7,20 +7,21 @@ public class Main {
     public static void main(String[] args) {
 
 	if (args.length != 1) {
-	    System.out.println("Please enter the bots token as the first argument e.g java -jar watanabot-1.0.jar <token>");
+	    System.out.println("Please enter the bot token as the first ");
 	    return;
 	}
-	
+
 	// Initializing commandHandler
 	CommandHandler commandHandler = new CommandHandler();
 
 	IDiscordClient cli = BotUtils.getBuiltDiscordClient(args[0]);
 
-	// Register a listener via the EventSubscriber annotation which allows for organisation and delegation of events
-        cli.getDispatcher().registerListener(commandHandler);
+	// Register a listener via the EventSubscriber annotation which allows for
+	// organisation and delegation of events
+	cli.getDispatcher().registerListener(commandHandler);
 
-        // Only login after all events are registered otherwise some may be missed.
-        cli.login();
+	// Only login after all events are registered otherwise some may be missed.
+	cli.login();
 
     }
 
