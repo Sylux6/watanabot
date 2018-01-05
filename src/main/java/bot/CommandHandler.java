40fad4140079ba0da3619c4b24f96a96f30c98a9
orvus <aurelien_du_77380@hotmail.fr>
2018-01-05 12:56:24 +0100

    comm handler ok

--- src/main/java/bot/CommandHandler.java
+++ src/main/java/bot/CommandHandler.java
@@ -14,57 +14,56 @@ import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedE
 
 public class CommandHandler {
 
-    // A map of modules mapping from module name to the map of commands
-    private Map<String, IModule> moduleMap;
+	// A map of modules mapping from module name to the map of commands
+	private Map<String, IModule> moduleMap;
 
-    public CommandHandler() {
-	moduleMap = new HashMap<>();
+	public CommandHandler() {
+		moduleMap = new HashMap<>();
 
-	// Init
-	moduleMap.put("general", new GeneralModule("general"));
-    }
-
-    @EventSubscriber
-    public void onMessageReceived(MessageReceivedEvent event) {
-
-	// Get all elements of the received message separated by blankspaces
-	String[] argArray = event.getMessage().getContent().split(" ");
-
-	// First ensure at least the command and prefix is present, the arg length can
-	// be handled by your command func
-	if (argArray.length == 0)
-	    return;
-
-	// Check if the first arg (the command) starts with the prefix defined in the utils class
-	if (!argArray[0].startsWith(BotUtils.BOT_PREFIX))
-	    return;
-
-	// Extract the module name or command part
-	String firstPart = argArray[0].substring(2);
-	firstPart.replace("//s+", ""); // Remove useless whitespaces (if the user didn't insert blankspace between
-				       // prefix and command we want to handle it anyway)
-
-	// Get the module map of commands
-	IModule module = moduleMap.get(firstPart);
-
-	// No specific module found
-	if (module == null) {
-	    module = moduleMap.get("general");
+		// Init
+		moduleMap.put("general", new GeneralModule("general"));
 	}
 
-	// Load the rest of the message in the array into a List for safer access
-	List<String> argsList = new ArrayList<>(Arrays.asList(argArray));
-	if (firstPart.length() == 0) {
-	    argsList.remove(0); // Remove the module name
-	} else {
-	    argsList.set(0, firstPart);
+	@EventSubscriber
+	public void onMessageReceived(MessageReceivedEvent event) {
+
+		// Get all elements of the received message separated by blankspaces
+		List<String> tmp = new ArrayList<>(Arrays.asList(event.getMessage().getContent().split("\\s+")));
+
+		// First ensure at least the command and prefix is present, the arg length can
+		// be handled by your command func
+		if (tmp.size() == 0)
+			return;
+
+		// Check if the first arg (the command) starts with the prefix defined in the
+		// utils class
+		if (!tmp.get(0).startsWith(BotUtils.BOT_PREFIX))
+			return;
+
+		List<String> argTab;
+
+		if (tmp.get(0).equals(BotUtils.BOT_PREFIX)) {
+			argTab = tmp.subList(1, tmp.size());
+		} else {
+			argTab = tmp;
+			argTab.set(0, argTab.get(0).substring(BotUtils.BOT_PREFIX.length()));
+		}
+
+		// Get the module map of commands
+		IModule module = moduleMap.get(argTab.get(0));
+
+		// No specific module found
+		if (module == null) {
+			module = moduleMap.get("general");
+		}else {
+			if (argTab.size() < 2 )
+				return;
+			argTab.remove(0);
+		}
+		// Instead of delegating the work to a switch, automatically do it via calling
+		// the mapping if it exists
+		
+		if (module.getCommands().containsKey(argTab.get(0)))
+			module.getCommands().get(argTab.get(0)).runCommand(event, argTab);
 	}
-
-	// Instead of delegating the work to a switch, automatically do it via calling
-	// the mapping if it exists
-
-	if (module.getCommands().containsKey(argsList.get(0)))
-	    module.getCommands().get(argsList.get(0)).runCommand(event, argsList);
-
-    }
 }
