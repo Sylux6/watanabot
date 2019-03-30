e6ef0da4282f49f961f240918f1da7deb2ff9898
Sylvain Ung <9683460+Sylux6@users.noreply.github.com>
2019-03-31 00:59:26 +0100

    add azur lane module

--- src/main/java/core/listeners/MessageListener.java
+++ src/main/java/core/listeners/MessageListener.java
@@ -22,63 +22,63 @@ public class MessageListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
 
-	// Ignore messages from BOT
-	if (event.getMessage().getAuthor().isBot())
-	    return;
+		// Ignore messages from BOT
+		if (event.getMessage().getAuthor().isBot())
+			return;
 
-	// Get all elements of the received message separated by blankspaces
-	List<String> tmp = new ArrayList<String>(Arrays.asList(event.getMessage().getContentDisplay().split("\\s+")));
+		// Get all elements of the received message separated by blankspaces
+		List<String> tmp = new ArrayList<>(Arrays.asList(event.getMessage().getContentDisplay().split("\\s+")));
 
-	// First ensure at least the command and prefix is present, the arg length can
-	// be handled by your command func
-	if (tmp.size() == 0)
-	    return;
+		// First ensure at least the command and prefix is present, the arg length can
+		// be handled by your command func
+		if (tmp.size() == 0)
+			return;
 
-	// Check if the first arg (the command) starts with the prefix defined in the
-	// utils class
-	if (!tmp.get(0).startsWith(BotUtils.BOT_PREFIX)) {
-	    if (event.getMessage().getMentionedUsers().contains(event.getJDA().getSelfUser()))
-		CommandHandler.service.execute(new ThreadMentionBehaviour(event));
-	    else
-		CommandHandler.service.execute(new ThreadGeneralBehaviour(event));
-	    return;
-	}
+		// Check if the first arg (the command) starts with the prefix defined in the
+		// utils class
+		if (!tmp.get(0).startsWith(BotUtils.BOT_PREFIX)) {
+			if (event.getMessage().getMentionedUsers().contains(event.getJDA().getSelfUser()))
+			CommandHandler.service.execute(new ThreadMentionBehaviour(event));
+			else
+			CommandHandler.service.execute(new ThreadGeneralBehaviour(event));
+			return;
+		}
 
-	List<String> argTab;
+		List<String> argTab;
 
-	if (tmp.get(0).equals(BotUtils.BOT_PREFIX)) {
-	    argTab = tmp.subList(1, tmp.size());
-	} else {
-	    argTab = tmp;
-	    argTab.set(0, argTab.get(0).substring(BotUtils.PREFIX_LENGTH));
-	}
+		if (tmp.get(0).equals(BotUtils.BOT_PREFIX)) {
+			argTab = tmp.subList(1, tmp.size());
+		} else {
+			argTab = tmp;
+			argTab.set(0, argTab.get(0).substring(BotUtils.PREFIX_LENGTH));
+		}
 
-	// Get the module map of commands
-	AbstractModule module;
+		// Get the module map of commands
+		AbstractModule module;
 
-	// No specific module found
-	if ((module = CommandHandler.moduleMap.get(argTab.get(0))) == null) {
-	    module = CommandHandler.moduleMap.get("general"); // Take general module by default
-	} else {
-	    if (argTab.size() < 2) // Message contains at least <module> <command>
-		return;
-	    argTab.remove(0); // Module name removed
-	}
-	
-	if (module instanceof MusicModule) { // Block MusicModule commands if Blindtest is running
-	    BlindtestModule blindtestModule = (BlindtestModule) CommandHandler.moduleMap.get("blindtest");
-	    if (blindtestModule.getBlindtestInstance(event.getGuild()) != null) {
-		BotUtils.sendMessage(event.getChannel(), "A blindtest game is running");
-		return;
-	    }
-	}
+		// No specific module found
+		if ((module = CommandHandler.moduleMap.get(argTab.get(0))) == null) {
+			module = CommandHandler.moduleMap.get("general"); // Take general module by default
+		} else {
+			if (argTab.size() < 2) // Message contains at least <module> <command>
+			return;
+			argTab.remove(0); // Module name removed
+		}
 
-	// Calling the command
-	if (module.getMapCommands().containsKey(argTab.get(0))) {
-	    CommandHandler.service.execute(new ThreadCommand(module, event, argTab));
-	} else if (module instanceof PictureModule) { // Getting image by default if unknown command
-	    CommandHandler.service.execute(new ThreadPictureDefaultBehaviour(event, argTab));
-	}
+		if (module instanceof MusicModule) { // Block MusicModule commands if Blindtest is running
+			BlindtestModule blindtestModule = (BlindtestModule) CommandHandler.moduleMap.get("blindtest");
+			if (blindtestModule.getBlindtestInstance(event.getGuild()) != null) {
+			BotUtils.sendMessage(event.getChannel(), "A blindtest game is running");
+			return;
+			}
+		}
+
+		// Calling the command
+		if (module.getMapCommands().containsKey(argTab.get(0))) {
+			CommandHandler.service.execute(new ThreadCommand(module, event, argTab));
+		} else if (module instanceof PictureModule) { // Getting image by default if unknown command
+			CommandHandler.service.execute(new ThreadPictureDefaultBehaviour(event, argTab));
+		}
     }
     
 }
--- src/main/java/modules/picture/PictureModule.java
+++ src/main/java/modules/picture/PictureModule.java
@@ -15,35 +15,35 @@ public class PictureModule extends AbstractModule {
 
     @Override
     public void populate() {
-	commands.put("nsfw", (event, args) -> {
-	    if (args.size() < 2) {
-		BotUtils.sendMessage(event.getChannel(), "I do not know what to get for you");
-		return;
-	    }
-	    if (event.getTextChannel().isNSFW())
-		getImage(event, String.join(" ", args.subList(1, args.size())), Rating.EXPLICIT);
-	    else
-		BotUtils.sendMessage(event.getChannel(), "You are not in a nsfw channel. You lewd!");
-	});
-	
-	commands.put("questionable", (event, args) -> {
-	    if (args.size() < 2) {
-		BotUtils.sendMessage(event.getChannel(), "I do not know what to get for you");
-		return;
-	    }
-	    if (event.getTextChannel().isNSFW())
-		getImage(event, String.join(" ", args.subList(1, args.size())), Rating.QUESTIONABLE);
-	    else
-		BotUtils.sendMessage(event.getChannel(), "You are not in a nsfw channel. You lewd!");
-	});	
+		commands.put("nsfw", (event, args) -> {
+			if (args.size() < 2) {
+			BotUtils.sendMessage(event.getChannel(), "I do not know what to get for you");
+			return;
+			}
+			if (event.getTextChannel().isNSFW())
+			getImage(event, String.join(" ", args.subList(1, args.size())), Rating.EXPLICIT);
+			else
+			BotUtils.sendMessage(event.getChannel(), "You are not in a nsfw channel. You lewd!");
+		});
 
-	commands.put("safe", (event, args) -> {
-	    if (args.size() < 2) {
-		BotUtils.sendMessage(event.getChannel(), "I do not know what to get for you");
-		return;
-	    }
-	    getImage(event, String.join(" ", args.subList(1, args.size())), Rating.SAFE);
-	});
+		commands.put("questionable", (event, args) -> {
+			if (args.size() < 2) {
+			BotUtils.sendMessage(event.getChannel(), "I do not know what to get for you");
+			return;
+			}
+			if (event.getTextChannel().isNSFW())
+			getImage(event, String.join(" ", args.subList(1, args.size())), Rating.QUESTIONABLE);
+			else
+			BotUtils.sendMessage(event.getChannel(), "You are not in a nsfw channel. You lewd!");
+		});
+
+		commands.put("safe", (event, args) -> {
+			if (args.size() < 2) {
+			BotUtils.sendMessage(event.getChannel(), "I do not know what to get for you");
+			return;
+			}
+			getImage(event, String.join(" ", args.subList(1, args.size())), Rating.SAFE);
+		});
     }
 
     static public void getByDefault(MessageReceivedEvent event, List<String> args) {
@@ -80,7 +80,7 @@ public class PictureModule extends AbstractModule {
 		} else if (rating == Rating.EXPLICIT) { // If no explicit image has been found, try again with
 							// questionable rating
 		    getImage(event, search, Rating.QUESTIONABLE);
-		} else if (rating == Rating.EXPLICIT) { // If no questionable image has been found, try again with safe
+		} else if (rating == Rating.QUESTIONABLE) { // If no questionable image has been found, try again with safe
 							// rating
 		    getImage(event, search, Rating.SAFE);
 		} else
