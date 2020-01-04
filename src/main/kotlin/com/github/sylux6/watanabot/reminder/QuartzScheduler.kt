96138daa1504d5312b6f5203064094b98911cb9a
Sylvain Ung <9683460+Sylux6@users.noreply.github.com>
2020-01-04 19:54:31 +0100

    Add Maia-nee birthday

--- src/main/kotlin/com/github/sylux6/watanabot/reminder/QuartzScheduler.kt
+++ src/main/kotlin/com/github/sylux6/watanabot/reminder/QuartzScheduler.kt
@@ -15,9 +15,11 @@ object QuartzScheduler {
         val startTime = DateBuilder.nextGivenSecondDate(null, 15)
 
         val youBirthday = newTrigger().startAt(startTime).withSchedule(cronSchedule("0 0 0 17 4 ? *")).build()
+        val maiaBirthday = newTrigger().startAt(startTime).withSchedule(cronSchedule("0 0 0 14 3 ? *")).build()
         val dailyTrigger = newTrigger().startAt(startTime).withSchedule(cronSchedule("0 0 0 * * ?")).build()
 
         sched.scheduleJob(newJob(WatanabeYou::class.java).build(), youBirthday)
+        sched.scheduleJob(newJob(Maia::class.java).build(), maiaBirthday)
         sched.scheduleJob(newJob(Batch::class.java).build(), dailyTrigger)
         sched.start()
     }
