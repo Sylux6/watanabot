8152578c1a5027f1c426ca7bf96d58d40752c965
Sylvain Ung <9683460+Sylux6@users.noreply.github.com>
2020-02-18 22:01:12 +0100

    Fix upsert in database

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ apply(plugin = "jarmonica")
 plugins {
     application
     java
-    `maven-publish`
+    maven
     kotlin("jvm") version "1.3.61"
     id("com.github.johnrengelman.shadow") version "5.2.0"
     id("org.jmailen.kotlinter") version "2.3.0"
